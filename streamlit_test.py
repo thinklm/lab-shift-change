4edefc5c3c9f455aff681f452007dcfe23df895c
Emanuel M. Lima <manumoncao@gmail.com>
2021-12-29 14:55:59 -0300

    Update streamlit_test.py
    Inserir dados OK para teste
    Possível mudança futura no layout: mensagem de erro para aparecer perto do botão de submit (aparece no topo da página)

--- streamlit_test.py
+++ streamlit_test.py
@@ -15,10 +15,10 @@ from google.cloud import firestore
 from datetime import datetime
 import json
 import streamlit as st
+from streamlit.type_util import Key
 st.set_page_config(layout="wide")
 
 ### DB CONN
-#db = firestore.Client.from_service_account_json("firestore_key.json")
 key_dict = json.loads(st.secrets["textkey"])
 db = firestore.Client.from_service_account_info(key_dict)
 
@@ -26,19 +26,19 @@ db = firestore.Client.from_service_account_info(key_dict)
 
 ### FUNCTIONS
 
-def db_check() -> None:
-    # Authenticate to Firestore with the JSON account key.
-    db = firestore.Client.from_service_account_json("firestore_key.json")
+# def db_check() -> None:
+#     # Authenticate to Firestore with the JSON account key.
+#     db = firestore.Client.from_service_account_json("firestore_key.json")
 
-    # Create a reference to the Google post.
-    doc_ref = db.collection("fechamentos").document("28122021A")
+#     # Create a reference to the Google post.
+#     doc_ref = db.collection("fechamentos").document("28122021A")
 
-    # Then get the data at that reference.
-    doc = doc_ref.get()
+#     # Then get the data at that reference.
+#     doc = doc_ref.get()
 
-    # Let's see what we got!
-    st.write("The id is: ", doc.id)
-    st.write("The contents are: ", doc.to_dict())
+#     # Let's see what we got!
+#     st.write("The id is: ", doc.id)
+#     st.write("The contents are: ", doc.to_dict())
 
 
 
@@ -72,8 +72,8 @@ def get_shift(now: datetime, mode: str="current") -> str:
 
 def upload_shift_data(submit_args: dict) -> None:
     now = datetime.now()
-    previous_shift = get_shift(now, mode='previous')
-    new_id  = now.strftime("%d%m%Y") + previous_shift
+    new_id  = now.strftime("%d%m%Y") + st.session_state.sft
+    submit_args["date"] = now
 
     doc_ref = db.collection("fechamentos").document(new_id)
     doc_ref.set(submit_args)
@@ -82,71 +82,90 @@ def upload_shift_data(submit_args: dict) -> None:
 
 
 
-def _submit_callback(submit_args: dict) -> None:
-    try:
-        df = pd.DataFrame(submit_args, index=[0])
-    except Exception as e:
-        st.error(f'Erro ao salvar dados:\n\n{e}')
+def _submit_callback() -> None:
+    
+    if st.session_state.sft == "Selecione":
+        st.error("Turno não selecionado!")
+    else:   
+        submit_args = {
+            "endedshift": st.session_state.sft,
+            "washer1": st.session_state.w1,
+            "washer1pend": st.session_state.w1p,
+            "sos1": st.session_state.s1,
+            "sos1pend": st.session_state.s1p,
+            "uvbc1": st.session_state.u1,
+            "uvbc1pend": st.session_state.u1p,
+            "washer2": st.session_state.w2,
+            "washer2pend": st.session_state. w2p,
+            "sos2": st.session_state.s2,
+            "sos2pend": st.session_state.s2p,
+            "uvbc2": st.session_state.u2,
+            "uvbc2pend": st.session_state.u2p,
+        }
 
-    # Mensagem de sucesso
-    upload_shift_data(submit_args)
-    st.success("Dados enviados com sucesso!")
+        
+        # Sobe os dados do turno para o banco de dados
+        upload_shift_data(submit_args)
 
-    # Mostra os dados efetuados
-    st.dataframe(df)
-
-
-
+        # Mensagem de sucesso
+        st.success("Dados enviados com sucesso!")
+        
+        # Limpa os campos de entrada pro usuário
+        clear_list = ["sft", "w1", "w1p", "s1", "s1p", "u1", "u1p", "w2", "w2p", "s2", "s2p", "u2", "u2p"]
+        for key in clear_list:
+            if key not in st.session_state.keys():
+                st.error("Chave inexistente")
+            elif key == "sft":
+                st.session_state[key] = "Selecione"
+            else:
+                st.session_state[key] = ""
 
 
 
 def _inserir_dados() -> None:
-    st.header("Inserir Dados")
 
+    st.header("Inserir Dados")
+    col_shift, col_empty  = st.columns([1,5])
+    with col_shift:
+        st.selectbox(label="Turno: ", options=["Selecione", "A", "B", "C"], key="sft")
+    with col_empty:
+        st.empty()
+    
+    
+    
     # FORMS
-    with st.form(key='form1', clear_on_submit=True):
+    with st.form(key='form1', clear_on_submit=False):
         col1, col2 = st.columns([3,3])
 
         with col1:
             st.subheader("Linha 571")
-            washer1 = st.text_area("Lavadora", placeholder="Lavadora da Linha 571")
-            sos1 = st.text_area("SOS", placeholder="SOS da Linha 571")
-            uvbc1 = st.text_area("UVBC", placeholder="UVBC da Linha 571")
+            st.text_area("Lavadora", placeholder="Lavadora da Linha 571", key="w1")
+            st.text_area("", placeholder="Pendências", key="w1p")
+            st.text_area("SOS", placeholder="SOS da Linha 571", key="s1")
+            st.text_area("", placeholder="Pendências", key="s1p")
+            st.text_area("UVBC", placeholder="UVBC da Linha 571", key="u1")
+            st.text_area("", placeholder="Pendências", key="u1p")
 
         with col2:
-            st.subheader("Linha 572")
-            selected_shift = st.selectbox(label="Turno: ", options=["Nenhum", "A", "B", "C"])
-            washer2 = st.text_area("Lavadora", placeholder="Lavadora da Linha 572")
-            sos2 = st.text_area("SOS", placeholder="SOS da Linha 572")
-            uvbc2 = st.text_area("UVBC", placeholder="UVBC da Linha 572")
-                
-        submit_button = st.form_submit_button(label="Enviar")            
+            st.subheader("Linha 572")            
+            st.text_area("Lavadora", placeholder="Lavadora da Linha 572", key="w2")
+            st.text_area("", placeholder="Pendências", key="w2p")
+            st.text_area("SOS", placeholder="SOS da Linha 572", key="s2")
+            st.text_area("", placeholder="Pendências", key="s2p")
+            st.text_area("UVBC", placeholder="UVBC da Linha 572", key="u2")
+            st.text_area("", placeholder="Pendências", key="u2p")
 
-        if submit_button:
-            if selected_shift == "Nenhum":
-                st.error("Turno não selecionado!")
-            else:
-                submit_args = {
-                    "endedshift": selected_shift,
-                    "washer1": washer1,
-                    "sos1": sos1,
-                    "uvbc1": uvbc1,
-                    "washer2": washer2,
-                    "sos2": sos2,
-                    "uvbc2": uvbc2
-                }
-                _submit_callback(submit_args=submit_args)
+        st.form_submit_button(label="Enviar", on_click=_submit_callback)
 
 
 
+# def display_current_time() -> None:
+#     now = datetime.now()
+#     #current_time = now.strftime("%H:%M:%S")
+#     current_date = now.strftime("%d/%m/%Y")
+#     current_shift = get_shift(now, mode='current')
 
-def display_current_time() -> None:
-    now = datetime.now()
-    #current_time = now.strftime("%H:%M:%S")
-    current_date = now.strftime("%d/%m/%Y")
-    current_shift = get_shift(now, mode='current')
-
-    st.write(f"Dia: {current_date} \nTurno: {current_shift}")
+#     st.write(f"Dia: {current_date} \nTurno: {current_shift}")
 
 
 
